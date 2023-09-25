442c41d0d283a8ea117a686b81ee8bd4e0f64bbf
Eshan Srivastava <eshan,srivastava273@gmail.com>
2023-09-25 22:35:13 +0530

    added codegen and new schema with cypher

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,29 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
+import { resolvers } from './resolvers';
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
 
 const typeDefs = readFileSync("./schema.graphql", {encoding: "utf-8"});
 
 //for now the queries are to be kept here
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
 
-const { url } = await startStandaloneServer( server, {
-    listen: {port: 4000},
-});
+const driver = neo4j.driver(
+    "bolt://localhost:7687",
+    neo4j.auth.basic("neo4j", "letmein")
+);
 
-console.log(`🚀  Server ready at: ${url}`);
+const neoSchema = new Neo4jGraphQL({ typeDefs, resolvers, driver });
+
+neoSchema.getSchema().then(async (schema) => {
+    const server = new ApolloServer({
+        schema: schema,
+    });
+    const { url } = await startStandaloneServer( server, {
+        listen: {port: 4000},
+    });
+    
+    console.log(`🚀  Server ready at: ${url}`);
+})
+
